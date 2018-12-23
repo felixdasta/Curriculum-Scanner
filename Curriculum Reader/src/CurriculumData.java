aa776bb7156412f1bcfab346ebe4e4a9a770ae65
Felix <Felix@FelixDasta-HP>
2018-12-23 01:52:47 -0400

    Code optimization and bug fixes

--- Curriculum Reader/src/CurriculumData.java
+++ Curriculum Reader/src/CurriculumData.java
@@ -19,32 +19,19 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 
-public class CurriculumData {
-	
-	private Scanner curriculumScan;
-	private String fileToScan;
+public class CurriculumData implements ActionListener, ItemListener{
+
+	private String fileName;
 	private LinkedList<String>coursesCodes;
-	private LinkedList<Integer>coursePositionerList;
 	private LinkedList<Integer>coursesCredits;
 	private LinkedList<String>coursesNames;
-	private LinkedList<String>y1sm1;
-	private LinkedList<String>y1sm2;
-	private LinkedList<String>y2sm1;
-	private LinkedList<String>y2sm2;
-	private LinkedList<String>y3sm1;
-	private LinkedList<String>y3sm2;
-	private LinkedList<String>y4sm1;
-	private LinkedList<String>y4sm2;
-	private LinkedList<String>y5sm1;
-	private LinkedList<String>y5sm2; 
 	private LinkedList<String>semesterSelection;
 	private ArrayList<String>takenCourses;
-	private JFrame frame = new JFrame();
-	private JComboBox currentSemester;
-	private JList leftlist;
-	private JList rightlist;
-	private JLabel lltitle;
-	private JLabel rltitle;
+	private ArrayList<LinkedList<String>>individualSemesters;
+	private JFrame frame;
+	private JComboBox <String>selectedSemester;
+	private JList<String> leftlist;
+	private JList<String> rightlist;
 	private JLabel creditsRepresentation;
 	private JButton markCourse;
 	private JButton writeFile;
@@ -52,207 +39,71 @@ public class CurriculumData {
 	private PrintWriter curriculumProgress;
 	private int totCredits;
 	private int takenCredits;
-	private int coursePositionerToAdd;
-	private int pos1;
-	private int pos2;
-	private int sociocount;
-	private int inglcount;
-	
+
 	public CurriculumData(String fileName)throws FileNotFoundException{
-		
-		fileToScan = fileName;
-		
-		setCurriculumData(fileToScan);
-		
-		frame.setTitle(fileName.substring(0, fileName.indexOf(".")));
+
+		this.fileName = fileName;
+
+		setCurriculumData(fileName);
+
+		frame = new JFrame(fileName.substring(0, fileName.indexOf(".")));
 		frame.setLayout(new FlowLayout());
 		frame.setSize(900, 150);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new JLabel("Required Courses: "));
 		
-		lltitle = new JLabel("Required Courses: ");
-		frame.add(lltitle);
-		
-		currentSemester = new JComboBox(semesterSelection.toArray());
-		frame.add(currentSemester);
-		
-		leftlist = new JList(coursesCodes.toArray());
+		selectedSemester = new JComboBox<String>(semesterSelection.toArray(new String[semesterSelection.size()]));
+		frame.add(selectedSemester);
+
+		leftlist = new JList<String>(coursesCodes.toArray(new String[coursesCodes.size()]));
 		leftlist.setVisibleRowCount(4);
 		leftlist.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		leftlist.setFixedCellWidth(100);
 		leftlist.setFixedCellHeight(15);
 		frame.add(new JScrollPane(leftlist));
-		
+
 		markCourse = new JButton("Mark as " + '"' + "Taken Courses" + '"');
 		frame.add(markCourse);
-		
-		rltitle = new JLabel("Taken Courses: ");
-		frame.add(rltitle);
-		
-		rightlist = new JList();
+
+		frame.add(new JLabel("Taken Courses: "));
+
+		rightlist = new JList<String>();
 		rightlist.setVisibleRowCount(4);
 		rightlist.setFixedCellWidth(100);
 		rightlist.setFixedCellHeight(15);
 		frame.add(new JScrollPane(rightlist));
-		
+
 		writeFile = new JButton("Write Curriculum Progress");
 		frame.add(writeFile);
-		
+
 		resetData = new JButton("Reset Data");
 		frame.add(resetData);
-		
+
 		creditsRepresentation = new JLabel(String.format("Curriculum Total Credits: %d \nTaken Credits: %d", totCredits, takenCredits));
 		frame.add(creditsRepresentation);
-		
-		currentSemester.addItemListener(new ItemListener(){
-			public void itemStateChanged(ItemEvent event){
-				currentSemesterStateChange();
-			}
-		}
-		);
-		courseHandling handler = new courseHandling();
-		markCourse.addActionListener(handler);
-		writeFile.addActionListener(handler);
-		resetData.addActionListener(handler);
-		
+
+		selectedSemester.addItemListener(this);
+		markCourse.addActionListener(this);
+		writeFile.addActionListener(this);
+		resetData.addActionListener(this);
+
 		frame.setVisible(true);
-
 	}
-	private void currentSemesterStateChange(){
-		if (currentSemester.getSelectedItem() == semesterSelection.toArray()[0]){
-			leftlist.setListData(coursesCodes.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[1]){
-			leftlist.setListData(y1sm1.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[2]){
-			leftlist.setListData(y1sm2.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[3]){
-			leftlist.setListData(y2sm1.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[4]){
-			leftlist.setListData(y2sm2.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[5]){
-			leftlist.setListData(y3sm1.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[6]){
-			leftlist.setListData(y3sm2.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[7]){
-			leftlist.setListData(y4sm1.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[8]){
-			leftlist.setListData(y4sm2.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[9]){
-			leftlist.setListData(y5sm1.toArray());
-		}
-		else if (currentSemester.getSelectedItem() == semesterSelection.toArray()[10]){
-			leftlist.setListData(y5sm2.toArray());
-		}
-		rightlist.setListData(takenCourses.toArray());
-	}
-	private class courseHandling implements ActionListener{
-			public void actionPerformed(ActionEvent event){
-				if(event.getSource() == markCourse){
-					for(Object o: leftlist.getSelectedValues()){
-						takenCourses.add(o.toString());
-					}
-					rightlist.setListData(takenCourses.toArray());
-					for(String course: takenCourses){
-						try{
-						int pos = coursesCodes.indexOf(course);
-						coursesCodes.remove(pos);
-						if (y1sm1.indexOf(course) != -1){
-							y1sm1.remove(y1sm1.indexOf(course));
-						}
-						else if (y1sm2.indexOf(course) != -1){
-							y1sm2.remove(y1sm2.indexOf(course));
-						}
-						else if (y2sm1.indexOf(course) != -1){
-							y2sm1.remove(y2sm1.indexOf(course));
-						}
-						else if (y2sm2.indexOf(course) != -1){
-							y2sm2.remove(y2sm2.indexOf(course));
-						}
-						else if (y3sm1.indexOf(course) != -1){
-							y3sm1.remove(y3sm1.indexOf(course));
-						}
-						else if (y3sm2.indexOf(course) != -1){
-							y3sm2.remove(y3sm2.indexOf(course));
-						}
-						else if (y4sm1.indexOf(course) != -1){
-							y4sm1.remove(y4sm1.indexOf(course));
-						}
-						else if (y4sm2.indexOf(course) != -1){
-							y4sm2.remove(y4sm2.indexOf(course));
-						}
-						else if (y5sm1.indexOf(course) != -1){
-							y5sm1.remove(y5sm1.indexOf(course));
-						}
-						else{
-							y5sm2.remove(y5sm2.indexOf(course));
-						}
-						takenCredits += coursesCredits.get(pos);
-						coursesCredits.remove(pos);
-						}catch(Exception e){
-							continue;
-					}
-				}
-				creditsRepresentation.setText(String.format("Curriculum Total Credits: %d Taken Credits: %d", totCredits, takenCredits));
-				currentSemesterStateChange();
-				}
-				else if (event.getSource() == writeFile){
-					try {
-						curriculumProgress = new PrintWriter("My Curriculum Progress.txt");
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				else if(event.getSource() == resetData){
-					try {
-						setCurriculumData(fileToScan);
-						creditsRepresentation.setText(String.format("Curriculum Total Credits: %d Taken Credits: %d", totCredits, takenCredits));
-						currentSemesterStateChange();
-					} catch (FileNotFoundException e) {
-						JOptionPane.showMessageDialog(null, "ERROR: Make sure that the curriculum text file is still available.");
-						e.printStackTrace();
-					}
 
-				}
-			}
-		}
 	private void setCurriculumData(String fileName)throws FileNotFoundException{
-		curriculumScan = new Scanner(new File(fileName));
-		
-		semesterSelection = new LinkedList<>();
-		takenCourses = new ArrayList<>();
-		coursesCodes = new LinkedList<>();
-		coursePositionerList = new LinkedList<>();
-		coursesCredits = new LinkedList<>();
-		coursesNames = new LinkedList<>();
-		y1sm1 = new LinkedList<>();
-		y1sm2 = new LinkedList<>();
-		y2sm1 = new LinkedList<>();
-		y2sm2 = new LinkedList<>();
-		y3sm1 = new LinkedList<>();
-		y3sm2 = new LinkedList<>();
-		y4sm1 = new LinkedList<>();
-		y4sm2 = new LinkedList<>();
-		y5sm1 = new LinkedList<>();
-		y5sm2 = new LinkedList<>();
-		totCredits = 0;
-		takenCredits = 0;
-		coursePositionerToAdd = 0;
-		sociocount = 1;
-		inglcount = 1;
-		
+		Scanner curriculumScan = new Scanner(new File(fileName));
+		LinkedList<Integer> coursePositionerList = new LinkedList<Integer>();
+
+		init();
+
+		int sociocount = 1;
+		int inglcount = 1;
+		int coursePositionerToAdd = 0;
+
 		semesterSelection.add("All Semesters");
-		
+
 		while(curriculumScan.hasNext()){
 			String line = curriculumScan.nextLine();
 			if (line.toUpperCase().startsWith("First".toUpperCase()) || line.toUpperCase().startsWith("Second".toUpperCase()) 
@@ -260,6 +111,7 @@ public class CurriculumData {
 					|| line.toUpperCase().startsWith("Fifth".toUpperCase())){
 				semesterSelection.add(line.substring(0, line.length() - 1));
 				coursePositionerList.add(coursePositionerToAdd);
+				individualSemesters.add(new LinkedList<String>());
 			}
 			else if(line.lastIndexOf(":") == line.length()-1){
 				continue;
@@ -268,75 +120,111 @@ public class CurriculumData {
 				continue;
 			}
 			else if (line.startsWith("SOCIO")){
-				pos1 = 0;
-				pos2 = line.indexOf(":");
+				int pos1 = 0;
+				int pos2 = line.indexOf(":");
 				coursesNames.add(line.substring(pos1, pos2));
 				coursesCodes.add("SOCIOXXXX" + "[" + sociocount + "]");
 				coursesCredits.add(Integer.parseInt(line.substring(line.lastIndexOf("-") + 2, line.lastIndexOf("-") + 3)));
 				totCredits += coursesCredits.get(coursesCredits.size() - 1);
-				sociocount += 1;
+				sociocount++;
 			}
 			else if (line.startsWith("English")){
-				pos1 = 0;
-				pos2 = line.indexOf(":");
+				int pos1 = 0;
+				int pos2 = line.indexOf(":");
 				coursesNames.add(line.substring(pos1, pos2));
 				coursesCodes.add("INGLXXXX" + "[" + inglcount + "]");
 				coursesCredits.add(Integer.parseInt(line.substring(line.indexOf("-") + 2, line.indexOf("-") + 3)));
 				totCredits += coursesCredits.get(coursesCredits.size() - 1);
-				inglcount += 1;
+				inglcount++;
 			}
 			else{
-				pos1 = line.indexOf(":") + 2;
-				pos2 = pos1 + 8;
+				int pos1 = line.indexOf(":") + 2;
+				int pos2 = pos1 + 8;
 				coursesNames.add(line.substring(0, pos1 - 2));
 				coursesCodes.add(line.substring(pos1, pos2));
 				coursesCredits.add(Integer.parseInt(line.substring(line.indexOf("-") + 2, line.indexOf("-") + 3)));
 				totCredits += coursesCredits.get(coursesCredits.size() - 1);
 			}
-			coursePositionerToAdd += 1;
+			coursePositionerToAdd++;
 		}
-		
+
 		curriculumScan.close();
 		
-		//Year I: Semester I Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(0), coursePositionerList.get(1) - 1).toArray()){
-				y1sm1.add(o.toString());
+		//WARNING: Watch the first GitHub Commit to understand better what is happening on the following lines of code
+		for(LinkedList<String> currentSemester: individualSemesters){
+			int posNumber;
+
+			if(individualSemesters.indexOf(currentSemester) != individualSemesters.size()-1)  posNumber = coursePositionerList.get(individualSemesters.indexOf(currentSemester) + 1) - (individualSemesters.indexOf(currentSemester) + 1);
+			else posNumber = coursesCodes.size();
+
+			for(Object o: coursesCodes.subList(coursePositionerList.get(individualSemesters.indexOf(currentSemester)) - individualSemesters.indexOf(currentSemester), posNumber).toArray()){
+				currentSemester.add(o.toString());
 			}
-		//Year I: Semester II Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(1) - 1, coursePositionerList.get(2) - 2).toArray()){
-				y1sm2.add(o.toString());
-			}
-		//Year II: Semester I Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(2) - 2, coursePositionerList.get(3) - 3).toArray()){
-				y2sm1.add(o.toString());
-			}
-		//Year II: Semester II Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(3) - 3, coursePositionerList.get(4) - 4).toArray()){
-				y2sm2.add(o.toString());
-			}
-		//Year III: Semester I Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(4) - 4, coursePositionerList.get(5) - 5).toArray()){
-				y3sm1.add(o.toString());
-			}
-		//Year III: Semester II Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(5) - 5, coursePositionerList.get(6) - 6).toArray()){
-				y3sm2.add(o.toString());
-			}
-		//Year IV: Semester I Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(6) - 6, coursePositionerList.get(7) - 7).toArray()){
-				y4sm1.add(o.toString());
-			}
-		//Year IV: Semester II Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(7) - 7, coursePositionerList.get(8) - 8).toArray()){
-				y4sm2.add(o.toString());
-			}
-		//Year V: Semester I Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(8) - 8, coursePositionerList.get(9) - 9).toArray()){
-				y5sm1.add(o.toString());
-			}
-		//Year V: Semester II Courses
-			for(Object o: coursesCodes.subList(coursePositionerList.get(9) - 9, coursesCodes.size()).toArray()){
-				y5sm2.add(o.toString());
-			}
+		}
 	}
+
+	private void init(){
+		semesterSelection = new LinkedList<>();
+		takenCourses = new ArrayList<>();
+		coursesCodes = new LinkedList<>();
+		coursesCredits = new LinkedList<>();
+		coursesNames = new LinkedList<>();
+		individualSemesters = new ArrayList<>();
+		totCredits = 0;
+		takenCredits = 0;
 	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public void actionPerformed(ActionEvent event){
+		if(event.getSource() == markCourse){
+			for(Object o: leftlist.getSelectedValues()){
+				takenCourses.add(o.toString());
+			}
+			for(String course: takenCourses){
+				try{
+					int pos = coursesCodes.indexOf(course);
+					coursesCodes.remove(pos);
+					for(LinkedList<String> currentSemester: individualSemesters){
+						if (currentSemester.contains(course)){
+							currentSemester.remove(course);
+						}
+					}
+					takenCredits += coursesCredits.get(pos);
+					coursesCredits.remove(pos);
+				}catch(Exception e){
+					continue;
+				}
+			}
+			creditsRepresentation.setText(String.format("Curriculum Total Credits: %d Taken Credits: %d", totCredits, takenCredits));
+			itemStateChanged((ItemEvent)selectedSemester.getAction());
+		}
+		else if (event.getSource() == writeFile){
+			try {
+				curriculumProgress = new PrintWriter("My Curriculum Progress.txt");
+				curriculumProgress.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		else if(event.getSource() == resetData){
+			try {
+				setCurriculumData(fileName);
+				creditsRepresentation.setText(String.format("Curriculum Total Credits: %d Taken Credits: %d", totCredits, takenCredits));
+				itemStateChanged((ItemEvent)selectedSemester.getAction());
+			} catch (FileNotFoundException e) {
+				JOptionPane.showMessageDialog(null, "ERROR: Make sure that the curriculum text file is still available.");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		if(selectedSemester.getSelectedItem() != semesterSelection.get(0))
+			leftlist.setListData(individualSemesters.get(semesterSelection.indexOf(selectedSemester.getSelectedItem())-1).toArray(new String[individualSemesters.get(semesterSelection.indexOf(selectedSemester.getSelectedItem())-1).size()]));
+		else
+			leftlist.setListData(coursesCodes.toArray(new String[coursesCodes.size()]));
+		rightlist.setListData(takenCourses.toArray(new String[takenCourses.size()]));
+	}
+}
--- Curriculum Reader/src/ProgramRunner.java
+++ Curriculum Reader/src/ProgramRunner.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class ProgramRunner {
 	public static void main(String args[])throws FileNotFoundException{
 		String fileName = JOptionPane.showInputDialog("Enter the name of the curriculum you wish to scan: ");
-		CurriculumData curriculumRunner = new CurriculumData(fileName);
+		new CurriculumData(fileName);
 		
 	}
 }
